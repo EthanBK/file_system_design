88af1ebf03a7869048944da6763121c6b8645e55
EthanBK <bhou@ucdavis.edu>
2019-02-25 13:57:14 -0800

    "some code"

--- src/Client.py
+++ src/Client.py
@@ -1,10 +1,29 @@
 import rpyc
-<<<<<<< HEAD:src/Client.py
-c=rpyc.connect('localhost',9487)
-print (c.root.sum(1,2))
-c.close()
-=======
-c=rpyc.connect('localhost',2222)
-print (c.root.sum(1,2))
-c.close()
->>>>>>> 63063a28ae7e7f71a461d56978bfa812bd941994:src/fsd_client.py
+import sys
+import os
+import argparse
+
+def put(source, target):
+    print(source, target)
+    pass
+
+
+if __name__ == "__main__":
+    # Parse in arguments
+    parser = argparse.ArgumentParser(description="Distributed File SYstem Client")
+    parser.add_argument('-o', '--operation', required=True, help="Select operation: put or get")
+    parser.add_argument('-s', '--source', required=True, help="Source file location")
+    parser.add_argument('-t', '--target', required=True, help="File mount point")
+    args = parser.parse_args()
+
+    # Build connection
+    con = rpyc.connect("localhost", port=2220)
+
+
+    if args.operation == 'put':
+        put(args.source, args.target)
+
+
+
+
+
--- src/MainServer.py
+++ src/MainServer.py
@@ -2,6 +2,12 @@ import rpyc
 import socket
 from rpyc.utils.server import ThreadedServer
 
+def set_config():
+    config = configparser.ConfigParser()
+    config.read('configure.ini')
+    block_size = int()
+
+
 
 class SBMainServer(rpyc.Service):
     def exposed_sum(self,a,b):
@@ -10,6 +16,8 @@ class SBMainServer(rpyc.Service):
 
 
 if __name__ == "__main__":
+    set_config()
+
     HOST = socket.gethostbyname(socket.gethostname())
     print(HOST)
     s=ThreadedServer(SBMainServer,port=9487,auto_register=False)
