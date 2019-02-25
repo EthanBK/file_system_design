0654183a4de87e08c7c31d325c1c7890e6d1d727
EthanBK <bhou@ucdavis.edu>
2019-02-25 14:37:47 -0800

    Parse configre in main server

--- src/MainServer.py
+++ src/MainServer.py
@@ -1,12 +1,17 @@
 import rpyc
-import socket
-from rpyc.utils.server import ThreadedServer
+# import socket
+# from rpyc.utils.server import ThreadedServer
+import configparser
+import os
 
 def set_config():
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    cf = os.path.join(THIS_FOLDER, 'configure.conf')
     config = configparser.ConfigParser()
-    config.read('configure.ini')
-    block_size = int()
-
+    config.readfp(open(cf))
+    block_size = int(config.get('mainServer', 'block_size'))
+    replication_factor = int(config.get('mainServer', 'replication_factor'))
+    # print(block_size, replication_factor)
 
 
 class SBMainServer(rpyc.Service):
@@ -18,7 +23,7 @@ class SBMainServer(rpyc.Service):
 if __name__ == "__main__":
     set_config()
 
-    HOST = socket.gethostbyname(socket.gethostname())
-    print(HOST)
-    s=ThreadedServer(SBMainServer,port=9487,auto_register=False)
-    s.start()
+    # HOST = socket.gethostbyname(socket.gethostname())
+    # print(HOST)
+    # s=ThreadedServer(SBMainServer,port=9487,auto_register=False)
+    # s.start()
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+# from rpyc.uti
