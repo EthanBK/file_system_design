c7a22297f4e59f8bbe7d757b26eedd475a9e12da
zzc558 <chezhou@ucdavis.edu>
2019-02-25 21:09:57 -0800

    Chen update SubServer

--- src/SubServer.py
+++ src/SubServer.py
@@ -2,32 +2,33 @@ import rpyc
 import uuid
 import os
 
-from rpyc.utils.server import ThreaderServer
+from rpyc.utils.server import ThreadedServer
 
 FILE_DIR = "/tmp/subserver/"
 
 class SubService(rpyc.Service):
+
+    def on_connect(self, conn):
+        print("subserver running...")
+
+    def on_disconnect(self, conn):
+        print("subserver terminated")
+
     class exposed_Subserver():
-<<<<<<< HEAD
-        def expoed_delete_file
-
-
-
-        def exposed_write(uuid, data, numSubs):
-            f = open(uuid, "x")
-            f.write(data)
-            if len(numSub) >= 1
-                self.storeTo(uuid, data)
-
-        def exposed_storeTo(uuid, data, numSubs):
-            host, port = numSub[0]
-            numSub = numSub[1:]
-            self.write(uuid, data, numSub)
-            
-            
-
-=======
         chunks = {}
+
+        def exposed_write(self, block_id, data, numSubs):
+            """write data to the block in each subserver"""
+            f = open(FILE_DIR + block_id, "x")
+            f.write(data)
+
+        def exposed_read(self, block_id):
+            """Return block content"""
+            block_address = File_DIR + str(block_id)
+            if not os.path.isfile(block_address):
+                return None
+            fname = open(block_address)
+            return fname.read()
 
         def exposed_delete_file(self, block_id):
             """Remove block with block_id"""
@@ -36,4 +37,11 @@ class SubService(rpyc.Service):
                 return None
             os.remove(block_address)
             return True
->>>>>>> 4b6cbc4bc368e528291f11012794d70b3c51958e
+
+if __name__ == "__main__":
+    if not os.path.isdir(FILE_DIR):
+        os.mkdir(FILE_DIR)
+        
+    subserver_port = input("Input the port for the Subserver: ")
+    sub  = ThreadedServer(SubService(), port = int(subserver_port))
+    sub.start()
