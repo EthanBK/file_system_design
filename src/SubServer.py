87d18cb04b12f7693d0fd1a44108989b98a1f28f
PearlHsu <ysihsu@ucdavis.edu>
2019-02-25 20:30:27 -0800

    git commit

--- src/SubServer.py
+++ src/SubServer.py
@@ -7,3 +7,19 @@ from rpyc.utils.server import ThreaderServer
 class SubService(rpyc.Service):
     class exposed_Subserver():
         def expoed_delete_file
+
+
+
+        def exposed_write(uuid, data, numSubs):
+            f = open(uuid, "x")
+            f.write(data)
+            if len(numSub) >= 1
+                self.storeTo(uuid, data)
+
+        def exposed_storeTo(uuid, data, numSubs):
+            host, port = numSub[0]
+            numSub = numSub[1:]
+            self.write(uuid, data, numSub)
+            
+            
+
