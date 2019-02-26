52943ac4f6515027fc9466d64e52102dcbb5a462
zzc558 <chezhou@ucdavis.edu>
2019-02-25 19:37:25 -0800

    Chen add delete function to SubServer.py

--- src/SubServer.py
+++ src/SubServer.py
@@ -4,6 +4,16 @@ import os
 
 from rpyc.utils.server import ThreaderServer
 
+FILE_DIR = "/tmp/subserver/"
+
 class SubService(rpyc.Service):
     class exposed_Subserver():
-        def expoed_delete_file
+        chunks = {}
+
+        def exposed_delete_file(self, block_id):
+            """Remove block with block_id"""
+            block_address = FILE_DIR + str(block_id)
+            if not os.path.isfile(block_address):
+                return None
+            os.remove(block_address)
+            return True
