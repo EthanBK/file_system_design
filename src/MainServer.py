a393c2b67da0a4c94f6e1f2b06295f97c2309ef7
EthanBK <bhou@ucdavis.edu>
2019-02-25 17:51:27 -0800

    ethan check point

--- src/MainServer.py
+++ src/MainServer.py
@@ -3,26 +3,57 @@ import rpyc
 # from rpyc.utils.server import ThreadedServer
 import configparser
 import os
+import math
+import uuid
+import random
 
 def set_config():
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     cf = os.path.join(THIS_FOLDER, 'configure.conf')
     config = configparser.ConfigParser()
-    config.readfp(open(cf))
-    block_size = int(config.get('mainServer', 'block_size'))
-    replication_factor = int(config.get('mainServer', 'replication_factor'))
+    config.read(cf)
+    MainServerService.block_size = \
+        int(config.get('mainServer', 'block_size'))
+    MainServerService.replication_factor = \
+        int(config.get('mainServer', 'replication_factor'))
     # print(block_size, replication_factor)
+    
 
 
-class SBMainServer(rpyc.Service):
+class MainServerService(rpyc.Service):
+    def __init__(self):    
+        self.block_size = 0             # size of each block
+        self.replication_factor = 0     # number of replicates of each block
+        self.subserver = {}             # unique id for each subserver
+
     def exposed_sum(self,a,b):
         return a+b
+    
+    # Return the number of block needed for storing file of size ::size
+    def get_num_blocks(self, size):
+        return int(math.ceil()(float(size) / self.block_size))
+
+    def get_block_id(self, target, num_block):
+        for i in range(num_block):
+            # get id for each block
+            block_id = uuid.uuid1()
+            # get id for target sub server 
+            subserv_id = random.sample()
+
+
+
 
 
 
 if __name__ == "__main__":
     set_config()
-
+    port = 2220
+    s = rpyc.utils.server.ThreadedServer(MainServerService, port = port)
+    
+    print("IP: localhost")
+    print("Port: ", port)
+    print("starting main server service...")
+    s.start()
     # HOST = socket.gethostbyname(socket.gethostname())
     # print(HOST)
     # s=ThreadedServer(SBMainServer,port=9487,auto_register=False)
